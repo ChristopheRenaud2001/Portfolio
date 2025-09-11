5186f7a8c300802b5ce3cb1477f2d4119ed54d44
Christophe Renaud <christophe_renaud@outlook.com>
2025-09-11 10:58:46 -0400

    Added About Me section and made some changes to styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import "./footer.css";
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
+import { FaAt } from "react-icons/fa";
 
 export default function Footer() {
 	return (
 		<div className="place-items-end">
 			<hr />
-			<footer className="place-items-center opacity-60">
+			<footer className="place-items-center opacity-60 py-6">
 				<p>Don't hesitate to reach out!</p>
 				<div className="mt-3">
 					<BsLinkedin
@@ -23,6 +24,10 @@ export default function Footer() {
 						onClick={() =>
 							window.open("https://github.com/ChristopheRenaud2001", "_blank")
 						}
+					/>
+					<FaAt
+						className="w-6 h-6 inline-block mx-2 transition ease-in-out hover:scale-130 opacity-50 hover:opacity-80  hover:cursor-pointer"
+						onClick={() => window.open("mailto:christophe_renaud@outlook.com")}
 					/>
 				</div>
 			</footer>
--- src/sections/Me/Me.tsx
+++ src/sections/Me/Me.tsx
@@ -1,22 +1,46 @@
 import { BiSquare } from "react-icons/bi";
+import { useHasRendered } from "../../utils/hooks";
+import { useRef } from "react";
 
 function Me() {
+	const section1Ref = useRef<HTMLDivElement>(null);
+	const section1HasRendered = useHasRendered({
+		ref: section1Ref as React.RefObject<HTMLDivElement>,
+		lockOnceRendered: true,
+	});
 	return (
-		<div className="place-items-center">
-			<div className="py-10" style={{ fontFamily: "Prata" }}>
+		<div id="me" className="place-items-center">
+			<div className="h-screen flex justify-center items-center">
 				<div className="inline-block place-items-end">
 					<div className="w-fit">
-						<p className="lg:ps-5 lg:text-start text-center">Hello, I'm</p>
+						<p
+							className={`lg:ps-5 lg:text-start text-center text-xl transition-all ease-in-out duration-1200 ${section1HasRendered ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
+							ref={section1Ref}
+						>
+							Hello, I'm
+						</p>
 						<h1 className="whitespace-normal lg:text-end text-center">
-							<span className="block">Christophe</span>
-							<span className="block">Renaud</span>
+							<span
+								className={`block transition-all ease-in-out delay-250 duration-1200 ${section1HasRendered ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
+							>
+								Christophe
+							</span>
+							<span
+								className={`block transition-all ease-in-out delay-250 duration-1200 ${section1HasRendered ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
+							>
+								Renaud
+							</span>
 						</h1>
-						<p className="lg:text-end text-center">
+						<p
+							className={`lg:pe-5 lg:text-end text-center text-xl transition-all ease-in-out delay-500 duration-1200 ${section1HasRendered ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
+						>
 							Web developer and UI/UX enthusiast
 						</p>
 					</div>
 				</div>
-				<div className="lg:inline-block hidden">
+				<div
+					className={`lg:inline-block hidden transition-all ease-in-out delay-500 duration-1000 ${section1HasRendered ? "opacity-100 translate-x-0" : "opacity-0 translate-x-10"}`}
+				>
 					<BiSquare className="w-60 h-60" />
 				</div>
 			</div>
