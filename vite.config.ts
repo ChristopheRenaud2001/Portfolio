a2fb82d73863bfc8288c780c1a70fb6b73091f8a
ChristopheRenaud2001 <146498671+ChristopheRenaud2001@users.noreply.github.com>
2025-11-13 14:49:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/ChristopheRenaud2001/Portfolio/',
 	plugins: [reactRouter(), tailwindcss()],
 });
