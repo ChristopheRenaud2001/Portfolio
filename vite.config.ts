98cd1b3ea4333b222e9420f5d8a40f47bb855813
Christophe Renaud <christophe_renaud@outlook.com>
2025-11-18 19:20:14 -0500

    Corrected build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
-import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+const isDev = process.env.NODE_ENV !== "production";
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: "/portfolio-of-chris/",
-	plugins: [reactRouter(), tailwindcss()],
+	plugins: [
+		isDev ? (await import("@react-router/dev/vite")).reactRouter() : null,
+		tailwindcss(),
+		react(),
+	].filter(Boolean),
 });
